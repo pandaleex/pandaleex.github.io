c0ddc7780b33aea4e35f87b573ae24e91f8912fd
pandaleex <108262104+pandaleex@users.noreply.github.com>
2022-10-24 15:18:21 +1100

    Added event handler, got rid of forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,63 @@
-let pokemonList=[
+let pokemonRepository = (function () {
+  let repository = [
     {
-        name:'Bulbasaur',
-        height: 0.7,
-        type: ['grass','poison']
+      name: "Bulbasaur",
+      height: 0.7,
+      type: ["grass", "poison"],
     },
     {
-        name:'Ivysaur',
-        height: 1,
-        type: ['grass','poison']
+      name: "Ivysaur",
+      height: 1,
+      type: ["grass", "poison"],
     },
     {
-        name:'Venusaur',
-        height: 2,
-        type: ['grass','poison']
+      name: "Venusaur",
+      height: 2,
+      type: ["grass", "poison"],
     },
-];
+  ];
 
-//Using HTML within JavaScript so styling can be applied
-document.write('<div class="pokemonList">');
-//A for loop which states a Pokemon's name and weight
-for (let i = 0; i < pokemonList.length; i++) {
-	if (pokemonList[i].height > 1.5) {
-        document.write('<div class="pokemon--big">');
-		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - Wow, that\'s giant!') //Adds conditions for stating the giant size of the pokemon
-	} else if (pokemonList[i].height <= 1.5 && pokemonList[i].height >= 1) {
-        document.write('<div class="pokemon--mid">'); 
-		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')')
-	} else	if (pokemonList[i].height < 1) { 
-        document.write('<div class="pokemon--small">');
-		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - Aww, that\'s tiny!') //Adds conditions for stating the small size of the pokemon
-	}
-    document.write('</div>');}
-    document.write('</div>'); //Ends div element within HTML
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function getAll() {
+    return repository;
+  }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener('click', function(event) {
+      event.showDetails();
+    })
+  }
+  function showDetails(pokemon) {
+    console.log(pokemon)
+  }
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+  };
+})();
+
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
